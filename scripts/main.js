f9c8cd408200b051e10122d3e33fe7efe542b22b
MostaK23 <artemm.mostovoy@maail.ru>
2022-06-04 22:39:11 +0300

    movement hot spots

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,43 @@
-const image = './images/room.jpeg';
+const room1 = './images/room.jpeg';
+const room2 = './images/room2.jpg';
+
 const bgImage = './images/bg.jpg';
-const background = document.getElementById('panorama');
+
 
 pannellum.viewer('panorama', {
-  "type": "equirectangular",
-  "panorama": image,
-  "preview": bgImage,
-  "autoLoad": true,
-  "hotSpots": [
-    {
-        "pitch": -1,
-        "yaw": 72,
-        "type": "info",
-        "text": "Телевизор",
-        "URL": "https://artbma.org/"
+  "default": {
+    "firstScene": "room2",
+    "autoLoad": true
+  },
+  "scenes": {
+    "room1": {
+      "type": "equirectangular",
+      "panorama": room1,
+      "hotSpots": [
+        {
+          "pitch": 0,
+          "yaw": 125,
+          "type": "scene",
+          "text": "Комната 2",
+          "sceneId": "room2"
+        }
+      ]
+    },
+    "room2": {
+      "type": "equirectangular",
+      "panorama": room2,
+      "pitch": 4,
+      "yaw": -120,
+      "hotSpots": [
+        {
+          "pitch": 7,
+          "yaw": -115,
+          "type": "scene",
+          "text": "Комната 1",
+          "sceneId": "room1"
+        }
+      ]
     }
-  ]
+  }
 });
 
