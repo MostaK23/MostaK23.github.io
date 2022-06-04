a6a930d55454c9f96f4b0f2958f28d7198c1d1b3
MostaK23 <artemm.mostovoy@maail.ru>
2022-06-04 22:07:42 +0300

    first hot spot

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 const image = './images/room.jpeg';
+const bgImage = './images/bg.jpg';
+const background = document.getElementById('panorama');
 
 pannellum.viewer('panorama', {
   "type": "equirectangular",
-  "panorama": image
+  "panorama": image,
+  "preview": bgImage,
+  "autoLoad": true,
+  "hotSpots": [
+    {
+        "pitch": -1,
+        "yaw": 72,
+        "type": "info",
+        "text": "Телевизор",
+        "URL": "https://artbma.org/"
+    }
+  ]
 });
+
