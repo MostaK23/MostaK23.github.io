150cbbb59955722ef419971984bdd41bd226aae5
MostaK23 <artemm.mostovoy@maail.ru>
2022-06-05 12:29:01 +0300

    Added hits

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ import { Scene, HotSpotInfo, HotSpotScene } from './constructors.js';
 
 const room1 = new Scene('equirectangular', './images/room.jpeg', [
   new HotSpotInfo(-2, 5, 'Картина', 'https://www.youtube.com/watch?v=iik25wqIuFo'),
+  new HotSpotInfo(46, -6, 'Info', 'https://www.youtube.com/shorts/aTFI1zyfrdI'),
+  new HotSpotInfo(72, 0, 'Info', 'https://www.youtube.com/shorts/aTFI1zyfrdI'),
   new HotSpotScene(123, 0, 'Купаться', 'sea')
 ]);
 
@@ -12,6 +14,10 @@ const sea = new Scene('equirectangular', './images/sea.jpg', [
 pannellum.viewer('panorama', {
   "default": {
     "firstScene": "room1",
+    "autoLoad": true,
+    "autoRotate": -1,
+    "orientationOnByDefault": true,
+    
   },
   "scenes": {
     "room1": room1,
