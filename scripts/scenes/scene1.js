82da8799ff998058bfb7aae7902d8a3a9b68804b
MostaK23 <72255414+MostaK23@users.noreply.github.com>
2022-06-28 16:29:51 +0300

    Add files via upload

--- scripts/scenes/scene1.js
+++ scripts/scenes/scene1.js
@@ -5,10 +5,10 @@ const nextScene = new HotSpotScene(5, -5, 'Перейти', 'scene2');
 
 const yBounds = [-45, 25];
 
-const tabletDescription = 'Планшет';
-const serverDescription = 'Серверная';
-const boardDescription = 'Доска';
-const computerDescription = `ПК\nCPU: Intel Core i5-10400 2.9GHz\nRAM: 16GB`;
+const tabletDescription = `Графический планшет X-Pen Artist 22R Pro\n\nРазрешение: 1920x1080(16:9)`;
+const serverDescription = 'Серверная\nЛокально-вычислительная сеть и система видеонаблюдения в МГУ имени А.А. Кулешова';
+const boardDescription = 'Интерактивная система Ritter 86\n\nТип дисплея: ЖК с LED-подсветкой\nДиагональ: 86"\nРазрешение: 3840x2160@60Гц';
+const computerDescription = `ОПС Компьютер\n\nCPU: Intel Core i7-10700\nRAM: DDR4 16ГБ\nSSD: 240ГБ\nGPU: Intel UHD Graphics 630`;
 
 const tablet = new HotSpotInfo(23, -11, tabletDescription);
 const server = new HotSpotInfo(-12, -9, serverDescription);
--- scripts/scenes/scene2.js
+++ scripts/scenes/scene2.js
@@ -5,11 +5,11 @@ const nextScene = new HotSpotScene(-15, -5, 'Перейти', 'scene1');
 
 const yBounds = [-45, 17];
 
-const computerDescription = 'Компьютер';
-const boardDescription = 'Доска';
+const computerDescription =  `ОПС Компьютер\n\nCPU: Intel Core i7-10700\nRAM: DDR4 16ГБ\nSSD: 240ГБ\nGPU: Intel UHD Graphics 630`;
+const boardDescription = 'Интерактивная система Ritter 86\n\nТип дисплея: ЖК с LED-подсветкой\nДиагональ: 86"\nРазрешение: 3840x2160@60Гц';
 
 const computer = new HotSpotInfo(-110, -21, computerDescription);
-const board = new HotSpotInfo(-36, 2, boardDescription);
+const board = new HotSpotInfo(-36, -1, boardDescription);
 
 export const scene2 = new EquirectangleScene(panorama, yBounds,  [
   nextScene,
