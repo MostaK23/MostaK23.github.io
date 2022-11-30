c3f70df112d927f20a34d8f8768f8e68c405a716
MostaK23 <artemm.mostovoy@maail.ru>
2022-11-30 22:43:25 +0300

    Add spaces

--- 3d-scroll/js/app.js
+++ 3d-scroll/js/app.js
@@ -7,24 +7,23 @@ let frames = Array.from($frames);
 let zVals = [];
 
 window.onscroll = function() {
+	let top = document.documentElement.scrollTop;
+	let delta = lastPos - top;
 
-  let top = document.documentElement.scrollTop;
-  let delta = lastPos - top;
+	lastPos = top;
 
-  lastPos = top;
+	frames.forEach(function(n, i) {
+		zVals.push((i * zSpacing) + zSpacing);
+		zVals[i] += delta * -5.5;
 
-  frames.forEach(function(n, i) {
-    zVals.push((i * zSpacing) + zSpacing);
-    zVals[i] += delta * -5.5;
-    
-    let frame = frames[i];
-    let transform = `translateZ(${zVals[i]}px)`;
-    let opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
+		let frame = frames[i];
+		let transform = `translateZ(${zVals[i]}px)`;
+		let opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0;
 
-    frame.setAttribute('style', 
-      `transform: ${transform};
+		frame.setAttribute('style',
+			`transform: ${transform};
        opacity: ${opacity}`);
-  });
+	});
 
 }
 
@@ -36,15 +35,15 @@ let soundButton = document.querySelector('.soundbutton');
 let audio = document.querySelector('.audio');
 
 soundButton.addEventListener('click', e => {
-  soundButton.classList.toggle('paused');
-  audio.paused ? audio.play() : audio.pause();
+	soundButton.classList.toggle('paused');
+	audio.paused ? audio.play() : audio.pause();
 });
 
 
-window.onfocus = function() {
-  soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+window.onfocus = function () {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play();
 }
 
-window.onblur = function() {
-  audio.pause();
+window.onblur = function () {
+	audio.pause();
 }
